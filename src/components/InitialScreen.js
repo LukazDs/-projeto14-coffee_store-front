9953503805410688e17e5e64f8f4296b593141e1
Lucas Duarte <lukazds2016@gmail.com>
2022-07-08 15:31:17 -0300

    Exibindo logo da aplicação

--- src/components/InitialScreen.js
+++ src/components/InitialScreen.js
@@ -1,7 +1,27 @@
+import logo from "../assets/images/caf.jpg";
+import styled from "styled-components";
+
 function InitialScreen () {
     return (
-        <h1>InitialScreen</h1>
+        <Container>
+            <img src={logo} alt="logo"/>
+        </Container>
     )
 }
 
 export default InitialScreen;
+
+const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #ffffff;
+
+    img {
+        width: 274px;
+        height: 212px;
+        object-fit: cover;
+    }
+`;
